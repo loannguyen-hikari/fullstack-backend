26fc189acf6f3ab8a5dab0f6393dccceba45f59d
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-03 20:19:54 +0700

    video 19

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 const app = express();
-const port = 8080;
+
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World Loan!");
 });
 
 app.get("/test", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
