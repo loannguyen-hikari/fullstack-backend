917f556f04835ddd6a07998d209917b99e49e19b
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-06 12:56:45 +0700

    video 32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,20 @@
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
-  res.send("Hello World Loan with nodemon!");
-}
+  let users = [];
+  connection.query("SELECT * FROM Users u", function (err, results, fields) {
+    users = results;
+    console.log("results= ", results);
+
+    console.log("users=", users);
+    res.send(JSON.stringify(users));
+  });
+};
 
 const getTestPage = (req, res) => {
   res.render("sample.ejs");
-}
+};
 module.exports = {
   getHomePage,
-  getTestPage
-}
+  getTestPage,
+};
