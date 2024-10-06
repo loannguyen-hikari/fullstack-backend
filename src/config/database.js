166a844a6c6b6e43c9c4899c18b3282a77825303
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-06 13:06:11 +0700

    video 33

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 //create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
