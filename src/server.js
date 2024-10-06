6d93f1e08239258eb3b6808664ab21397ce887af
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-06 10:44:34 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -13,6 +14,22 @@ configViewEngine(app);
 
 //declare routes
 app.use("/", webRoutes);
+
+//test connection
+//create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "hoidanit",
+  port: 3307,
+  password: "123456",
+});
+
+//simple query
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+  console.log("results= ", results);
+  console.log("fields= ", fields);
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
