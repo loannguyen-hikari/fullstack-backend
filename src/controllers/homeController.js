754ffc9c00f85cf94ab881911d342c7cc87ad5bc
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-06 13:30:27 +0700

    video 35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,7 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("SELECT * FROM Users u", function (err, results, fields) {
-    users = results;
-    console.log("results= ", results);
-
-    console.log("users=", users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
 const getTestPage = (req, res) => {
