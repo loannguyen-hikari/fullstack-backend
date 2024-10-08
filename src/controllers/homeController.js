f4f8c95d3cb4c72aa71fcb79b04da748f7d52f48
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-08 18:50:05 +0700

    video 39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,26 +8,40 @@ const getTestPage = (req, res) => {
   res.render("sample.ejs");
 };
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
   let { email, myname, city } = req.body;
   console.log("email", email);
   console.log("name", myname);
   console.log("city", city);
 
-  connection.query(
+  // connection.query(
+  //   `INSERT INTO
+  //   Users (email, name, city)
+  //   VALUES (?,?,?)`,
+  //   [email, myname, city],
+  //   function (err, results) {
+  //     console.log(results);
+  //     res.send("Created user successfully");
+  //   }
+  // );
+
+  let [results, fields] = await connection.query(
     `INSERT INTO 
     Users (email, name, city)
     VALUES (?,?,?)`,
-    [email, myname, city],
-    function (err, results) {
-      console.log(results);
-      res.send("Created user successfully");
-    }
+    [email, myname, city]
   );
+
+  res.send("Created user successfully");
+};
+
+const getCreatePage = (req, res) => {
+  res.render("create.ejs");
 };
 
 module.exports = {
   getHomePage,
   getTestPage,
   postCreateUser,
+  getCreatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -3,13 +3,14 @@ const {
   getHomePage,
   getTestPage,
   postCreateUser,
+  getCreatePage,
 } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/", getHomePage);
 
 router.get("/test", getTestPage);
-
+router.get("/create", getCreatePage);
 router.post("/create-user", postCreateUser);
 
 module.exports = router;
