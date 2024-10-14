23a302c47ff89f65e23635da79baac90f686e508
loannguyen <nguyenloandhnn96@gmail.com>
2024-10-14 14:02:29 +0700

    video 86

--- src/routes/api.js
+++ src/routes/api.js
@@ -8,10 +8,18 @@ const {
   postUploadSingleAPI,
 } = require("../controllers/apiController");
 
+const { postCreateCustomerAPI } = require("../controllers/customerController");
+
+//routes for users
 routerAPI.get("/users", getUsersAPI);
 routerAPI.post("/users", postCreateUserAPI);
 routerAPI.put("/users", putUpdateUserAPI);
 routerAPI.delete("/users", deleteUserAPI);
+
+//route for uploading image
 routerAPI.post("/file", postUploadSingleAPI);
+
+//route for customers
+routerAPI.post("/customers", postCreateCustomerAPI);
 
 module.exports = routerAPI;
